2751314bc830c65caa429da8003d2ae1cc50c1ce
Kazuki H <58931401+Kaz205@users.noreply.github.com>
2022-09-15 22:34:11 +0900

    Add a script to fetch and compress Clang

---
+++ clang.sh
@@ -0,0 +1,12 @@
+if [ -z "$1" ]
+then
+   echo "No Clang version specified"
+   exit
+fi
+
+export CLANG_VERSION=clang-$1
+rm -rf linux-x86
+git clone https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 -b master --depth=1 -j$(nproc --all)
+cd linux-x86
+tar -C . -cf $CLANG_VERSION.tar $CLANG_VERSION
+xz -e -T8 -9 $CLANG_VERSION.tar
