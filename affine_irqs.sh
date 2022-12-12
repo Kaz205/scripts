63444c69b2595598dad082c79910f64e9bb3de1e
Kazuki H <58931401+Kaz205@users.noreply.github.com>
2022-12-12 10:49:02 +0900

    Add script to affine IRQs to CPUs

---
+++ affine_irqs.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# Usecase:
+# items[n]="'irq_name' 'CPU(s) affinity list'"
+
+items[0]="23510c000.spi 1"
+items[1]="206408000.mbox-recv 3"
+items[2]="206408000.mbox-send 2"
+items[3]="231c08000.mbox-recv 1"
+items[4]="231c08000.mbox-send 0"
+items[5]="nvme-apple 3"
+items[6]="brcmf_pcie_intr 1"
+
+# Don't allow non-root users to execute this
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+for item in "${items[@]}"
+do
+	# Get text before whitespace
+	IRQ_NAME=$(echo "$item" | awk '{print $1}')
+	# Get text until first occurance of colon and remove all whitespace (xargs)
+	IRQ_NUM=$(cat /proc/interrupts | grep $IRQ_NAME | cut -d: -f1 | xargs)
+	# Get text after whitespace
+	AFFINITY_NUM=$(echo "$item" | awk '{print $2}')
+	echo $AFFINITY_NUM > /proc/irq/$IRQ_NUM/smp_affinity_list
+	echo "Affined $IRQ_NAME to CPU$AFFINITY_NUM"
+done
+
