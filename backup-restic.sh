ba50be13fe69db7033c338c69eb5be99f1ee39ed
Kazuki Hashimoto <kazukih0205@gmail.com>
2024-12-13 22:01:39 +0900

    Update

--- backup-restic.sh
+++ backup-restic.sh
@@ -1,1 +1,7 @@
-/home/kazuki/restic --exclude={/dev,/media,/mnt,/proc,/run,/sys,/tmp,/var/tmp} --exclude="/home/kazuki/roms" --exclude="/home/kazuki/.cache" --exclude="/home/kazuki/.local/share/Trash" -r /mnt/data/restic-repo/ backup / --verbose
+if [[ $(hostname) == "azusa" ]]; then
+    repodir="sftp:kazuki@192.168.11.229:/mnt/data/restic-repo/"
+elif [[ $(hostname) == "hifumi" ]]; then
+    repodir="/mnt/data/restic-repo/"
+fi
+
+/home/kazuki/restic --exclude={/dev,/media,/mnt,/proc,/run,/sys,/tmp,/var/tmp} --exclude="/home/kazuki/roms" --exclude="/home/kazuki/.cache" --exclude="/home/kazuki/.local/share/Trash" -r $repodir backup / --verbose
